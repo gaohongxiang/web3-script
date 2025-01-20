b3fb3a2c22452ad516663c1ad37008a01927c251
高鸿祥 <gaohongxiang@gaohongxiangdeMacBook-Pro.local>
2025-01-20 14:06:35 +0800

    删除非必要函数

--- packages/utils-module/generateWallet.js
+++ packages/utils-module/generateWallet.js
@@ -1,4 +1,4 @@
-import fsp from 'fs/promises'; // 使用 promises API，并将导入名称更改为 fsp
+import fsPromises from 'fs/promises'; // 使用 promises API，并将导入名称更改为 fsPromises
 import bs58 from 'bs58';
 
 import * as bip39 from 'bip39';
@@ -26,14 +26,14 @@ export async function generateEthWallet(num = 10) {
 
   // 判断文件是否存在
   try {
-    await fsp.access(walletfile);
+    await fsPromises.access(walletfile);
   } catch {
     // 文件不存在则创建文件并写入标题行
     const header = 'indexId,ethAddress,enEthPrivateKey,enEthMnemonic\n';
-    await fsp.writeFile(walletfile, header);
+    await fsPromises.writeFile(walletfile, header);
   }
 
-  const file = await fsp.open(walletfile, 'a');
+  const file = await fsPromises.open(walletfile, 'a');
 
   for (let i = 1; i <= num; i++) {
     const wallet = ethers.Wallet.createRandom();
@@ -65,14 +65,14 @@ export async function generateBtcWallet(num = 10) {
 
   // 判断文件是否存在
   try {
-    await fsp.access(walletfile);
+    await fsPromises.access(walletfile);
   } catch {
     // 文件不存在则创建文件并写入标题行
     const header = 'indexId,btcAddress,enBtcPrivateKey,enBtcMnemonic\n';
-    await fsp.writeFile(walletfile, header);
+    await fsPromises.writeFile(walletfile, header);
   }
 
-  const file = await fsp.open(walletfile, 'a');
+  const file = await fsPromises.open(walletfile, 'a');
 
   for (let i = 1; i <= num; i++) {
     const network = bitcoin.networks.bitcoin; // 指定比特币网络
@@ -110,14 +110,14 @@ export async function generateSolWallet(num = 10) {
 
   // 判断文件是否存在
   try {
-    await fsp.access(walletfile);
+    await fsPromises.access(walletfile);
   } catch {
     // 文件不存在则创建文件并写入标题行
     const header = 'indexId,solAddress,enSolPrivateKey\n';
-    await fsp.writeFile(walletfile, header);
+    await fsPromises.writeFile(walletfile, header);
   }
 
-  const file = await fsp.open(walletfile, 'a');
+  const file = await fsPromises.open(walletfile, 'a');
 
   for (let i = 1; i <= num; i++) {
     const keyPair = solanaWeb3.Keypair.generate();
@@ -145,14 +145,14 @@ export async function generateSuiWallet(num = 10) {
 
   // 判断文件是否存在
   try {
-    await fsp.access(walletfile);
+    await fsPromises.access(walletfile);
   } catch {
     // 文件不存在则创建文件并写入标题行
     const header = 'indexId,suiAddress,enSuiPrivateKey\n';
-    await fsp.writeFile(walletfile, header);
+    await fsPromises.writeFile(walletfile, header);
   }
 
-  const file = await fsp.open(walletfile, 'a');
+  const file = await fsPromises.open(walletfile, 'a');
 
   for (let i = 1; i <= num; i++) {
     const keypair = new Ed25519Keypair();
@@ -178,14 +178,14 @@ export async function generatePassword(num = 10) {
 
   // 判断文件是否存在
   try {
-    await fsp.access(walletfile);
+    await fsPromises.access(walletfile);
   } catch {
     // 文件不存在则创建文件并写入标题行
     const header = 'indexId,enPassword\n';
-    await fsp.writeFile(walletfile, header);
+    await fsPromises.writeFile(walletfile, header);
   }
 
-  const file = await fsp.open(walletfile, 'a');
+  const file = await fsPromises.open(walletfile, 'a');
 
   for (let i = 1; i <= num; i++) {
     const randomPassword = generateRandomString(18);
@@ -199,40 +199,4 @@ export async function generatePassword(num = 10) {
 
   await file.close();
   console.log(`已将 ${num} 个钱包存储到 ${walletfile}`);
-}
-
-/**
- * 在现有的 CSV 文件中添加新列（加密密码）并补全值，并将结果写回到现有的 CSV 文件中。
- * @param {string} filePath - 要读取和更新的现有 CSV 文件路径。
- * @param {string} newColumnName - 要添加的新列的列名。
- */
-export async function addColumnAndPopulate(filePath, newColumnName) {
-  try {
-    const data = await fsp.readFile(filePath, 'utf8');
-
-    // 解析 CSV 文件内容
-    const rows = data.split('\n');
-    const header = rows.shift().split(',');
-
-    // 添加新列名
-    header.push(newColumnName);
-
-    // 更新每一行的值
-    for (let i = 0; i < rows.length; i++) {
-      const row = rows[i].split(',');
-      const randomPassword = generateRandomString(18);
-      const enPassword = await enCryptText(randomPassword);
-      row.push(enPassword);
-      rows[i] = row.join(',');
-    }
-
-    // 生成更新后的 CSV 文件内容
-    const updatedContent = [header.join(','), ...rows].join('\n');
-
-    // 写回到现有的 CSV 文件
-    await fsp.writeFile(filePath, updatedContent, 'utf8');
-    console.log('成功添加新列');
-  } catch (err) {
-    console.error('添加新列失败：', err);
-  }
 }
