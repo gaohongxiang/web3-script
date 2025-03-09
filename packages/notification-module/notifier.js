20dec58ff946eec19d169267f51f30005298f496
高鸿祥 <gaohongxiang@gaohongxiangdeMacBook-Pro.local>
2025-03-09 19:26:48 +0800

    使用重试机制和消息管理器优化代码

--- packages/notification-module/notifier.js
+++ packages/notification-module/notifier.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import 'dotenv/config';
+import { withRetry } from '../utils-module/retry.js';
 import { getCurrentTime } from '../utils-module/utils.js';
 /**
  * 发送钉钉通知消息
@@ -9,21 +10,19 @@ import { getCurrentTime } from '../utils-module/utils.js';
  * @throws {Error} 当发送消息失败时抛出错误
  */
 export async function dingdingNotifier(content, keyWord = 'web3消息通知') {
-    try {
-        const url = 'https://oapi.dingtalk.com/robot/send?access_token=' + process.env.dingtalkAccessToken;
-        const msg = {
-            "msgtype": "text",
-            "text": {"content": keyWord + '\n' + content + '\n预警时间: ' + getCurrentTime()}
-        };
-        
+    const url = 'https://oapi.dingtalk.com/robot/send?access_token=' + process.env.dingtalkAccessToken;
+    const msg = {
+        "msgtype": "text",
+        "text": { "content": keyWord + '\n' + content + '\n预警时间: ' + getCurrentTime() }
+    };
+
+    withRetry(async () => {
         const response = await axios.post(url, msg, {
-            headers: {"Content-Type": "application/json;charset=utf-8"},
+            headers: { "Content-Type": "application/json;charset=utf-8" },
             timeout: 10000  // 10秒超时
         });
-        console.error('钉钉通知信息发送成功');
         return response.data;
-    } catch (err) {
-        console.error('钉钉通知信息发送失败:', err);
-        throw err;
-    }
+    }, {
+        taskName: '钉钉通知信息发送',
+    });
 }
